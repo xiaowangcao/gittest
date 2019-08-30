d06c04f2673a795882a2423b1f2e3cd502a96587
chenzhaoping <564534188@qq.com>
2019-08-31 01:01:36 +0800

    在工作空间中修改test的输出

--- src/main/java/gittest/GitTest.java
+++ src/main/java/gittest/GitTest.java
@@ -4,5 +4,6 @@ public class GitTest {
 	public static void main(String[] args) {
 	  System.err.println(1+"22222");//修改为2222
 	  System.err.println(1+"   ");//并添加空格符
+	  System.err.println(1+"在工作空间里修改");
 	}
 }
